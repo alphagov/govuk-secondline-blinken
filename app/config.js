e9bfdca5e7afb87ff233f2bcfb1520d2389e236d
Tijmen Brommet <tijmen@gmail.com>
2017-11-02 12:36:49 +0000

    Commit config.js for GOV.UK
    I'm not sure how this app was ever able to run on Heroku without this
    config file present. The URLs in there are public knowledge.

---
+++ app/config.js
@@ -0,0 +1,22 @@
+var blinken_config = {
+  "groups": [
+    {
+      "id": "govuk",
+      "name": "",
+      "environments": [
+        {
+          "name": "Production",
+          "url": "https://alert.publishing.service.gov.uk"
+        },
+        {
+          "name": "Staging",
+          "url": "https://alert.staging.publishing.service.gov.uk"
+        },
+        {
+          "name": "Integration",
+          "url": "https://alert.integration.publishing.service.gov.uk"
+        }
+      ]
+    }
+  ]
+}
